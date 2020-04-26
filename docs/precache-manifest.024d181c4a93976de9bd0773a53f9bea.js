9f95e33dc30edf17641766511a00d48def7c3ced
Orie Steele <orie@transmute.industries>
2020-04-26 18:02:25 -0500

    :rocket:

--- docs/precache-manifest.024d181c4a93976de9bd0773a53f9bea.js
+++ docs/precache-manifest.024d181c4a93976de9bd0773a53f9bea.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "722e2b9b307e8e9bb85cb540b991466d",
+    "revision": "a2a3efabeee112e008cab5c2c0d70194",
     "url": "/index.html"
   },
   {
-    "revision": "b0b36ba6875b8f639b91",
+    "revision": "def94f4037207353467b",
     "url": "/static/css/main.77e16dfb.chunk.css"
   },
   {
-    "revision": "acc44a70200f3d3ef77b",
-    "url": "/static/js/2.853c3af2.chunk.js"
+    "revision": "ce43a30faaaeac0951e8",
+    "url": "/static/js/2.31896f47.chunk.js"
   },
   {
     "revision": "3adc01bea76e7956dc3633ee898f6936",
-    "url": "/static/js/2.853c3af2.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.31896f47.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b0b36ba6875b8f639b91",
-    "url": "/static/js/main.bf188472.chunk.js"
+    "revision": "def94f4037207353467b",
+    "url": "/static/js/main.e5b966ad.chunk.js"
   },
   {
     "revision": "6a839d995601f12e8c9c",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41dedecc9e0cfcf3629ef26d9fd831d6.js"
+  "/precache-manifest.024d181c4a93976de9bd0773a53f9bea.js"
 );
 
 self.addEventListener('message', (event) => {
