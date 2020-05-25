31e493f00f23e19d7f7c578ef6b8f31d077bf2ed
Orie Steele <orie@transmute.industries>
2020-05-25 11:40:09 -0500

    update config

--- packages/pwa/src/components/ScanQRCodeDialog/ScanQRCodeDialog.js
+++ packages/pwa/src/components/ScanQRCodeDialog/ScanQRCodeDialog.js
@@ -5,10 +5,10 @@ import Button from "@material-ui/core/Button";
 
 import Grid from "@material-ui/core/Grid";
 import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
-
-import DialogTitle from "@material-ui/core/DialogTitle";
 
 import { JSONEditor } from "@transmute/material-did-core";
 
